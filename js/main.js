a68199e9571665f78bffc1abd76f35cb66d465c3
An-ga-hee <heevel2@naver.com>
2022-04-28 12:17:54 +0900

    section5

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
+/* header */
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 800) {
+            $("header").css("backgroundColor", "#000000e6")
+        } else {
+            $("header").css("backgroundColor", "transparent");
+        }
+    })
+});
 
-        /* header */
-        $(function () {
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > 800) {
-                    $("header").css("backgroundColor", "#000000e6")
-                } else {
-                    $("header").css("backgroundColor", "transparent");
-                }
-            })
-        });
+/* top-btn scroll */
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#top-btn').fadeIn();
+        } else {
+            $('#top-btn').fadeOut();
+        }
+    });
+
+    $("#top-btn").click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 200);
+        return false;
+    });
+});
