6102722ba6908bdcb82152968f346b55f0a3b98b
An-ga-hee <heevel2@naver.com>
2022-05-02 18:07:33 +0900

    0502

--- js/main.js
+++ js/main.js
@@ -20,8 +20,7 @@ $(function () {
   });
 
   $("#top-btn").click(function () {
-    $("html, body").animate(
-      {
+    $("html, body").animate({
         scrollTop: 0,
       },
       200
@@ -29,7 +28,106 @@ $(function () {
     return false;
   });
 });
+/* typeIt */
+document.addEventListener("DOMContentLoaded", function () {
+  new TypeIt(".title")
+    .pause(500)
+    .go();
+});
 
+/* section1 slide */
+var swiper = new Swiper(".intro", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 25000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/* healing-slide */
+var swiper = new Swiper(".healing-cont", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/* nft-slide */
+var swiper = new Swiper(".artplatform-slide", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 200,
+  slidesPerView: "auto",
+  loop: true,
+  loopFillGroupWithBlank: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+/* menu-slide PC */
+var swiper = new Swiper(".menu-slide", {
+  slidesPerView: 4,
+  spaceBetween: 0,
+  grabCursor: true,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/* menu-slide mobile */
+var swiper = new Swiper(".menu-mobile", {
+  slidesPerView: 2,
+  spaceBetween: 0,
+  grabCursor: true,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 /* section3-popup */
 const modals = document.getElementsByClassName("popup-wrap");
 const btns = document.getElementsByClassName("image-grid__item");
@@ -40,13 +138,14 @@ function Modal(num) {
   return function () {
     btns[num].onclick = function () {
       modals[num].style.display = "block";
-      // console.log(num);
+      console.log(num);
     };
     spanes[num].onclick = function () {
       modals[num].style.display = "none";
+      console.log(num);
     };
   };
-}
+};
 
 for (var i = 0; i < btns.length; i++) {
   funcs[i] = Modal(i);
